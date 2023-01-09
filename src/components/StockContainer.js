19fa96f3f6fac7863582e3623c3df7fca4e941e4
levideutsch <levideutsch@live.com>
2023-01-09 13:15:33 -0500

    commit tree d90e05741600126e1b71561f568740929446d12c

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
-import Stock from "./Stock";
+import React, {useEffect} from "react";
+// import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
+
+//  function handleClick(e) {
+//   e.preventDefault()
+
+//   fetch()
+//  }
+  
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => {
+        return <div key={stock.id} className="card">
+          <div className="card-body">
+            <h5 className="card-title">{stock.name}</h5>
+            <p className="card-text">{stock.ticker}: {stock.price}</p>
+          </div>
+        </div>
+      })}
     </div>
   );
 }
