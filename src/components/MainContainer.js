822aa09afdd7bf8b062eb10d6ecb9b48f582daba
levideutsch <levideutsch@live.com>
2023-01-11 14:19:53 -0500

    stuff

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,14 +2,29 @@ import React from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
+import { useEffect, useState } from "react";
 
 function MainContainer() {
+
+const [data, setData] = useState([])
+
+// Rendering all stocks
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then((response) => response.json())
+    .then((data) => setData(data))
+  }, [])
+
+  function handleBought(stock) {
+    console.log("in bought list!", stock)
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+      <StockContainer stocks={data}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
